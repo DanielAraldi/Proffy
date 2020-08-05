1de0b4f9131304ea0c53bd379a11cdf6239ab0c5
Daniel <danielsaraldi@gmail.com>
2020-08-05 15:11:11 -0300

    Terminando o cadastro de novas aulas e o listamento de professores.
    Manutenções e algumas coisas adicionais também foram feitas.

--- web/src/components/TeacherItem/index.tsx
+++ web/src/components/TeacherItem/index.tsx
@@ -2,33 +2,52 @@ import React from 'react';
 
 import whatsappIcon from '../../assets/images/icons/whatsapp.svg';
 
+import api from '../../services/api';
+
 import './styles.css';
 
-function TeacherItem() {
+export interface Teacher {
+    id: number;
+    avatar: string;
+    bio: string;
+    cost: number;
+    name: string;
+    subject: string;
+    whatsapp: string;
+}
+
+interface TeacherItemProps {
+    teacher: Teacher;
+}
+
+const TeacherItem: React.FC<TeacherItemProps> = ({ teacher }) => {
+    function createNewConnection() {
+        api.post('connections', {
+            user_id: teacher.id,
+        })
+    }
+
     return (
         <article className="teacher-item">
             <header>
-                <img src="https://avatars0.githubusercontent.com/u/50931267?s=460&u=3110e194231c31ba4f7bcea3f2a7fd29d4e6afd7&v=4" alt="Daniel Sansão Araldi" />
+                <img src={teacher.avatar} alt={teacher.name} />
                 <div>
-                    <strong>Daniel Sansão Araldi</strong>
-                    <span>Matemática</span>
+                    <strong>{teacher.name}</strong>
+                    <span>{teacher.subject}</span>
                 </div>
             </header>
 
-            <p>Existem muitas variações disponíveis de passagens de Lorem Ipsum.
-            <br /><br />
-            Mas a maioria sofreu algum tipo de alteração, seja por inserção de passagens com humor, ou palavras aleatórias que não parecem nem um pouco convincentes.
-            </p>
+            <p>{teacher.bio}</p>
 
             <footer>
                 <p>
                     Preço/Hora
-                    <strong>R$80,00</strong>
+                <strong>R$ {teacher.cost}</strong>
                 </p>
-                <button type="button">
+                <a onClick={createNewConnection} href={`https://wa.me/${teacher.whatsapp}`} target="blank_">
                     <img src={whatsappIcon} alt="Whatsapp" />
-                            Entrar em contanto
-                        </button>
+                        Entrar em contanto
+                </a>
             </footer>
         </article>
     );
