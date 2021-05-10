e85c9a483ef185dc214f0c3abadd5453aaaa5fad
Daniel <danielsaraldi@gmail.com>
2021-05-10 20:02:15 -0300

    ci: some changes

--- server/src/database/connection.ts
+++ server/src/database/connection.ts
@@ -1,14 +1,12 @@
-import knex from 'knex'; // Permite você escreve sql por javascript
-import path from 'path';
-
-// migrations: Controlam a versão do banco de dados
+import knex from "knex";
+import path from "path";
 
 const db = knex({
-    client: 'sqlite3',
-    connection: {
-        filename: path.resolve(__dirname, 'database.sqlite')
-    },
-    useNullAsDefault: true, // Se o valor não ser informado é preechido com Null no banco de dados
+  client: "sqlite3",
+  connection: {
+    filename: path.resolve(__dirname, "database.sqlite"),
+  },
+  useNullAsDefault: true, // Se o valor não ser informado é preechido com Null no banco de dados
 });
 
 export default db;
