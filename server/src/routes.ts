0ea301746e216674ada24ce31271b164f2e1fb2f
Daniel <danielsaraldi@gmail.com>
2021-05-10 20:26:35 -0300

    feat: add throw error not found page

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -3,6 +3,8 @@ import express from "express";
 import { ClassesController } from "./controllers/ClassesController";
 import { ConnectionsController } from "./controllers/ConnectionsController";
 
+import { ApiError } from "./errors/ApiError";
+
 const routes = express.Router();
 
 const classesControllers = new ClassesController();
@@ -13,5 +15,9 @@ routes.post("/classes", classesControllers.create);
 
 routes.get("/connections", connectionsController.index);
 routes.post("/connections", connectionsController.create);
+
+routes.all("*", () => {
+  throw new ApiError("Not Found Page", 404);
+});
 
 export { routes };
