3869e23afa1b9e89fce9a281150fbb2a4c74e910
Daniel <danielsaraldi@gmail.com>
2021-05-10 21:03:48 -0300

    feat: create validator to connection created

---
+++ server/src/validators/connection-create-adapter.ts
@@ -0,0 +1,24 @@
+import { Request } from "express";
+import * as yup from "yup";
+
+import { ApiError } from "../errors";
+
+export class ConnectionCreateAdapter {
+  async isValidRequest(request: Request) {
+    const schema = yup.object({
+      user_id: yup
+        .number()
+        .integer()
+        .positive()
+        .strict(true)
+        .typeError("Value isn't number")
+        .required("Required user ID!"),
+    });
+
+    try {
+      await schema.validate(request.body, { abortEarly: false });
+    } catch (error) {
+      throw new ApiError("Some data are invalid!", 400);
+    }
+  }
+}
