0f7f69370cf96b060a7867b045ee1e61ef3f3e07
Daniel <danielsaraldi@gmail.com>
2021-05-10 21:24:27 -0300

    feat: implements validators in ConnectionController

--- server/src/controllers/ConnectionsController.ts
+++ server/src/controllers/ConnectionsController.ts
@@ -1,6 +1,10 @@
 import { Request, Response } from "express";
 
-import db from "../database/connection";
+import { db } from "../database/connection";
+
+import { ApiError } from "../errors";
+
+import { ConnectionCreateAdapter } from "../validators";
 
 export class ConnectionsController {
   async index(request: Request, response: Response) {
@@ -12,7 +16,19 @@ export class ConnectionsController {
   }
 
   async create(request: Request, response: Response) {
+    const connectionCreateAdapter = new ConnectionCreateAdapter();
+
+    await connectionCreateAdapter.isValidRequest(request);
+
     const { user_id } = request.body;
+
+    const userNotExists = await db
+      .select("id")
+      .from("users")
+      .where("id", user_id);
+
+    if (userNotExists.length === 0)
+      throw new ApiError("This user isn't exists", 400);
 
     await db("connection").insert({
       user_id,
